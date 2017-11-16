01c4256642137c4a85a1d77bca9ee8b42c883ffe
akorp <alpost@zoho.com>
2017-11-16 11:58:16 +0100

    added font roboto for material-ui

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import LoadDataElements from './FetchElemsExample';
+import 'typeface-roboto';
 
 
 //ReactDOM.render(<App />, document.getElementById('root'));
