c431e495ed9730199b3e0e6d8b374ab90a99a5d5
Dmitry <filosofov.dmitry@gmail.com>
2017-12-04 20:38:44 +0100

    formatting

--- src/containers/MapArea.js
+++ src/containers/MapArea.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import L from 'leaflet'
-import d2map from 'dhis2-gis-api'
+import React from "react";
+import PropTypes from "prop-types";
+import L from "leaflet";
+import d2map from "dhis2-gis-api";
 
 
 export default class MapArea extends React.Component {
   constructor(props) {
     super(props);
     let coordinates = [];
-    let selectedOrg = props.allUnits.find((org)=>{return org.id === props.levelOne[0].id});
+    let selectedOrg = props.allUnits.find((org) => {
+      return org.id === props.levelOne[0].id;
+    });
     if (selectedOrg.featureType === "POINT") {
       coordinates = this.prepPointCoords(selectedOrg.coordinates);
     } else if (selectedOrg.featureType === "POLYGON") {
@@ -19,19 +21,16 @@ export default class MapArea extends React.Component {
       coordinates: coordinates,
       featureType: selectedOrg.featureType
     };
-
-    // console.log(this.state);
-    // console.log(selectedOrg);
   }
 
   componentDidMount() {
-    if(this.state.featureType !== "NONE"){
+    if (this.state.featureType !== "NONE") {
       this.renderMap(this.state.coordinates, this.state.featureType);
     }
   }
 
-  componentDidUpdate(prevProps, prevState){
-    if(this.state.featureType !== "NONE"){
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.featureType !== "NONE") {
       this.renderMap(this.state.coordinates, this.state.featureType);
     }
   }
@@ -43,29 +42,26 @@ export default class MapArea extends React.Component {
     if (selectedOrg.featureType !== "NONE") {
       coordinates = JSON.parse(selectedOrg.coordinates);
       coordinates = this.getCoordinates(selectedOrg, coordinates);
-      if (selectedOrg.featureType === "POINT"){
-        // console.log("New coordinates", coordinates);
+      if (selectedOrg.featureType === "POINT") {
         this.setState({
           coordinates: coordinates,
           featureType: selectedOrg.featureType
         });
-      }else if(selectedOrg.featureType === "POLYGON"){
-        // console.log("POLYGON COORDS:", coordinates);
+      } else if (selectedOrg.featureType === "POLYGON") {
         this.setState({
           coordinates: coordinates,
           featureType: selectedOrg.featureType
         });
-      }else if (selectedOrg.featureType === "MULTI_POLYGON"){
-        // console.log(selectedOrg.coordinates);
+      } else if (selectedOrg.featureType === "MULTI_POLYGON") {
         this.setState({
           coordinates: coordinates,
           featureType: selectedOrg.featureType
         });
       }
-    }else if(selectedOrg.featureType === "NONE"){
+    } else if (selectedOrg.featureType === "NONE") {
       this.setState({
         featureType: "NONE"
-      })
+      });
     }
 
   }
@@ -81,19 +77,19 @@ export default class MapArea extends React.Component {
     return coordinates;
   }
 
-  prepPointCoords(point){
+  prepPointCoords(point) {
     return [point[1], point[0]];
   }
 
-  prepPolygonCoords(points){
+  prepPolygonCoords(points) {
     let map_area = [];
     for (let point of points[0][0]) {
-      map_area.push(this.prepPointCoords(point))
+      map_area.push(this.prepPointCoords(point));
     }
     return map_area;
   }
 
-  prepMultiPolygon(points){
+  prepMultiPolygon(points) {
     let map_areas = [];
     for (let polygon of points) {
       let poly = [];
@@ -106,47 +102,48 @@ export default class MapArea extends React.Component {
   }
 
   renderMap(coords, type) {
-    if(this.map === undefined){
-      this.map = L.map('map');
-    }else{
-      this.map.remove();
-      this.map = L.map('map');
-    }
-    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(this.map);
+    this.createOrUpdateMap();
 
-    //
-    if (type === "POLYGON"){
+    if (type === "POLYGON") {
       let polygon = L.polygon(coords).addTo(this.map);
       let bounds = polygon.getBounds();
       this.map.fitBounds(bounds);
-
-    }else if (type === "POINT"){
+    } else if (type === "POINT") {
       let ico = L.icon({
-        iconUrl: '/marker.png',
+        iconUrl: "/marker.png",
         iconSize: [25, 41],
         iconAnchor: [12, 41],
-        popupAnchor: [-3, -76]
       });
       L.marker(coords, {icon: ico}).addTo(this.map);
       this.map.setView(coords, 15);
-    }else if(type === "MULTI_POLYGON"){
+    } else if (type === "MULTI_POLYGON") {
       let polygonGroup = L.featureGroup();
-        for (let polygonCoordinates of coords) {
-            let polygon = L.polygon(polygonCoordinates);
-            polygonGroup.addLayer(polygon);
-        }
-        polygonGroup.addTo(this.map);
-        this.map.fitBounds(polygonGroup.getBounds());
+      for (let polygonCoordinates of coords) {
+        let polygon = L.polygon(polygonCoordinates);
+        polygonGroup.addLayer(polygon);
+      }
+      polygonGroup.addTo(this.map);
+      this.map.fitBounds(polygonGroup.getBounds());
     }
 
   }
 
 
+  createOrUpdateMap() {
+    if (this.map === undefined) {
+      this.map = L.map("map");
+    } else {
+      this.map.remove();
+      this.map = L.map("map");
+    }
+    L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+      attribution: "&copy; <a href=\"http://osm.org/copyright\">OpenStreetMap</a> contributors"
+    }).addTo(this.map);
+  }
+
   render() {
     let mapStyle = {
-      height: '600px'
+      height: "600px"
     };
 
     return (
@@ -157,8 +154,8 @@ export default class MapArea extends React.Component {
             <h3>Map:</h3>
             <div id="map" style={mapStyle}></div>
           </div>
-          )) ||
-          (<p>No map available for selected unit</p>)}
+        )) ||
+        (<p>No map available for selected unit</p>)}
       </div>
     );
   }
