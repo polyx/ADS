aaa4ac251734f3d690afc6d9edba33a92cde5870
akorp <alpost@zoho.com>
2017-12-05 22:46:06 +0100

    with data elemenets on infopage

--- src/containers/InfoSheetArea.js
+++ src/containers/InfoSheetArea.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {Tabs, Tab, PanelGroup, Panel, ListGroup, ListGroupItem, Button, ButtonToolbar} from "react-bootstrap";
+import {Tabs, Tab, PanelGroup, Panel, ListGroup, ListGroupItem, Table, Button, ButtonToolbar} from "react-bootstrap";
 import moment from 'moment';
 import {loadQuery} from '../api';
 import {isUserAdmin} from '../globals/UserInfo';
@@ -15,7 +15,7 @@ export default class InfoSheetArea extends React.Component {
       tabObj: [],
       tabDataSets: [],
       tabOrgUnitGroups: [],
-      tabPermissions: [],
+      tabDataElems: [],           // tabX - [[elemId, elemName, val], [elemId, elemName, val]]
     }
     this.handleTabSelect = this.handleTabSelect.bind(this);
     this.handlePanelSelect = this.handlePanelSelect.bind(this);
@@ -26,7 +26,7 @@ export default class InfoSheetArea extends React.Component {
     if (nextProps.selectedOrg !== null) {
       if ((this.state.tabObj[this.state.activeTabKey] === undefined) ||
       (this.state.tabObj[this.state.activeTabKey].id !== nextProps.selectedOrg.id)) {
-        console.log('handleNewSelectedOrg');
+        //console.log('handleNewSelectedOrg');
         this.handleNewSelectedOrg(nextProps.selectedOrg);
       } 
     }
@@ -34,27 +34,60 @@ export default class InfoSheetArea extends React.Component {
 
 
   async handleNewSelectedOrg(org) {
+    // fetching orgunit data
     let orgUnitObj = await loadQuery('organisationUnits/' + org.id + '.json');
     let tabObj = this.state.tabObj;
     tabObj[this.state.activeTabKey] = orgUnitObj;
-
+    // fetching dataSets
     let ids = ''.concat(orgUnitObj.dataSets.map((dataSet)=>{return dataSet.id}));
     let dataSets = await loadQuery(`/dataSets.json?paging=false&filter=id:in:[${ids}]&fields=id,displayName`);
     dataSets = dataSets.dataSets;
     let tabDataSets = this.state.tabDataSets;
     tabDataSets[this.state.activeTabKey] = dataSets;
-
+    // fetching organisationUnitGroups
     ids = ''.concat(orgUnitObj.organisationUnitGroups.map((group)=>{return group.id}));
     let orgUnitGroups = await loadQuery(`/organisationUnitGroups.json?paging=false&filter=id:in:[${ids}]&fields=id,displayName`);
     orgUnitGroups = orgUnitGroups.organisationUnitGroups;
     let tabOrgUnitGroups = this.state.tabOrgUnitGroups;
     tabOrgUnitGroups[this.state.activeTabKey] = orgUnitGroups;
+    // fetching data elements, indicators, year
+    let dataStorDataElems = await loadQuery(`/dataStore/ads_data_elems/${org.id}`);
+    //let dataStorDataElems = await loadQuery(`/dataStore/ads_data_elems/1111`);
+    //let dataStorDataElems = {'FTRrcoaog83':0, 'P3jJH5Tu5VC':0};
+    let dataElems;
+    if (dataStorDataElems !== null) {
+      dataElems = await this.fetchAnalyticsData(dataStorDataElems, org.id);
+    } else {
+      dataElems = null;
+    }
+    let tabDataElems = this.state.tabDataElems;
+    tabDataElems[this.state.activeTabKey] = dataElems;
+
+    console.log(dataElems);
 
     this.setState({
       tabObj: tabObj,
       tabDataSets: tabDataSets,
       tabOrgUnitGroups: tabOrgUnitGroups,
+      tabDataElems: tabDataElems,
     });
+  }
+
+  async fetchAnalyticsData(dStorDataElems, orgId){
+    let ou = orgId;
+    let pe = 2017;
+    let dx = Object.keys(dStorDataElems).join(';');
+    let analytics = await loadQuery(`26/analytics?paging=false&dimension=dx:${dx}&dimension=pe:${pe}&dimension=ou:${ou}`);
+    // console.log(analytics);
+    let dataElemsData = analytics.rows.map((row) => {
+      let arr = [];
+      arr.push(row[0]);
+      arr.push(row[3]);
+      arr.push(analytics.metaData.items[row[0]].name);
+      return arr;
+    });
+    // console.log(dataElemsData);
+    return dataElemsData;
   }
 
 
@@ -71,14 +104,13 @@ export default class InfoSheetArea extends React.Component {
         <Panel collapsible defaultExpanded={true} header="Basic Information" eventKey="0">{this.renderPanelBasic(tabKey)}</Panel>
         <Panel collapsible defaultExpanded={false} header="Data Sets" eventKey="1">{this.renderPanelDataSets(tabKey)}</Panel>
         <Panel collapsible defaultExpanded={false} header="Organization Unit Groups" eventKey="2">{this.renderPanelOrgGroups(tabKey)}</Panel>
-        <Panel collapsible header="Users" eventKey="4">{this.renderPanelUsers(tabKey)}</Panel>
+        <Panel collapsible defaultExpanded={false} header="Data Elements" eventKey="3">{this.renderPanelDataElems(tabKey)}</Panel>
         {obj && (<Panel collapsible header="Manage" eventKey="6">{this.renderManagePanel(obj)}</Panel>)}
       </PanelGroup>
     );
   }
 
   renderManagePanel(obj){
-    console.log(obj);
     return(
           <ButtonToolbar>
             <Link to={"/admin/"+obj.id}><Button>Manage</Button></Link>
@@ -130,13 +162,18 @@ export default class InfoSheetArea extends React.Component {
     );
   }
 
-  renderPanelUsers() {
+  renderPanelDataElems(tabKey) {
+    let dataElems = this.state.tabDataElems[tabKey];
+    console.log(dataElems);
+    let list = dataElems ?
+      dataElems.map((dataElems,i)=>{return (<ListGroupItem key={i}>{dataElems[2]}: <span style={{float: 'right'}}>{dataElems[1]}</span></ListGroupItem>)})
+      : null;
     return (
-      <ListGroup >
-     </ListGroup>
+      <ListGroup fill>
+        {list}          
+      </ListGroup>
     );
   }
-
 
 
   handleTabSelect(tabKey) {
@@ -147,10 +184,10 @@ export default class InfoSheetArea extends React.Component {
   }
 
   render() {
-    let styleTab = {padding: '1px, 1px, 1px 1px'};
+    //let styleTab = {padding: '1px, 1px, 1px 1px'};
     return(
       <div>
-        <Tabs activeKey={this.state.activeTabKey} onSelect={this.handleTabSelect} id="controlled-tab" style={styleTab}>
+        <Tabs activeKey={this.state.activeTabKey} onSelect={this.handleTabSelect} id="controlled-tab">
           <Tab eventKey={0} title={this.state.tabObj[0] ? this.state.tabObj[0].name : 'Tab1'}> {this.renderInfo(0)} </Tab>
           <Tab eventKey={1} title={this.state.tabObj[1] ? this.state.tabObj[1].name : 'Tab2'}> {this.renderInfo(1)} </Tab>
           <Tab eventKey={2} title={this.state.tabObj[2] ? this.state.tabObj[2].name : 'Tab3'}> {this.renderInfo(2)} </Tab>          
